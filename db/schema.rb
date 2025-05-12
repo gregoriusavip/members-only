7cdfa78effde81aa052ebe69ab432ee7d9fbdbef
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2025-05-12 15:24:50 -0700

    Add devise to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_12_202137) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_12_220157) do
   create_table "clubhouses", force: :cascade do |t|
     t.string "club_name", null: false
     t.datetime "created_at", null: false
@@ -28,12 +28,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_12_202137) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
-    t.string "password", null: false
-    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "clubhouse_id"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["clubhouse_id"], name: "index_users_on_clubhouse_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
