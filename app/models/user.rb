cc05f40f816d04eb34e5e131bcb3b358b322acdb
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2025-05-12 18:56:31 -0700

    Adjust validation field

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, :email, :password, presence: true
+  validates :username, :email, :encrypted_password, presence: true
   has_many :posts
   belongs_to :clubhouse
 end
