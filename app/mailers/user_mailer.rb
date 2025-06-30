4ce5f9ff1c6c472325ea8add6806995ddfff13de
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2025-06-29 21:40:03 -0700

    Create user mailer method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+  def welcome_email
+    @user = params[:user]
+    @url  = "http://127.0.0.1:3000/users/sign_in"
+    mail(to: @user.email, subject: "Welcome to My Awesome Site")
+  end
 end
